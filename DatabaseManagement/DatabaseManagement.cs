50d77e308d1cd0cea58abec03090b804a675eebd
GreedNeSS <ruslan199126@gmail.com>
2022-03-27 15:15:45 +0300

    Added database management example.

---
+++ DatabaseManagement/DatabaseManagement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DatabaseManagement.Models;
+
+namespace DatabaseManagement
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=users.db");
+        }
+    }
+}
---
+++ DatabaseManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseManagement;
+
+Console.WriteLine("***** Database Management *****");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    bool isAvailable = await db.Database.CanConnectAsync();
+    Console.WriteLine(isAvailable ? "\nБаза данных существует!" : "\nБаза данных не существует!");
+
+    bool isCreated = await db.Database.EnsureCreatedAsync();
+    Console.WriteLine(isCreated ? "\nБаза данных создана." : "\nБаза данных была создана раньше.");
+
+    isAvailable = await db.Database.CanConnectAsync();
+    Console.WriteLine(isAvailable ? "База данных существует!" : "База данных не существует!");
+
+    bool isDeleted = await db.Database.EnsureDeletedAsync();
+    Console.WriteLine(isDeleted ? "\nБаза данных удалена." : "\nБаза данных не была удалена.");
+
+    isAvailable = await db.Database.CanConnectAsync();
+    Console.WriteLine(isAvailable ? "База данных существует!" : "База данных не существует!");
+}
