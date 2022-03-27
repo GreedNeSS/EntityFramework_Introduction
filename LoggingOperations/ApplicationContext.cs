8c6a4b6bdfe624505e5ea97f8b2b62b8f2f08357
GreedNeSS <ruslan199126@gmail.com>
2022-03-27 22:15:52 +0300

    ***** Logging Operations *****

---
+++ LoggingOperations/ApplicationContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using LoggingOperations.Models;
+
+namespace LoggingOperations
+{
+    public class ApplicationContext: DbContext
+    {
+        private readonly StreamWriter _writer = new StreamWriter("log.txt", true);
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=users.db");
+            optionsBuilder.LogTo(message =>
+            {
+                Console.WriteLine(message);
+                _writer.WriteLine(message);
+            }
+            // OR
+            //,Microsoft.Extensions.Logging.LogLevel.Information
+
+            // OR
+            //, new[] { RelationalEventId.CommandExecuted }
+
+            // OR
+            //, new[] { DbLoggerCategory.Database.Transaction.Name }
+            
+            // OR
+            , new[] { DbLoggerCategory.Database.Command.Name }
+            );
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _writer.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await _writer.DisposeAsync();
+        }
+    }
+}
---
+++ LoggingOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using LoggingOperations;
+using LoggingOperations.Models;
+
+Console.WriteLine("***** Logging Operations *****");
+
+User user1 = new User { Name = "GreedNeSS", Age = 30 };
+User user2 = new User { Name = "Marcus", Age = 45 };
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.Add(user1);
+    db.Add(user2);
+    db.SaveChanges();
+
+    var users = db.Users.ToList();
+
+    foreach (var u in users)
+    {
+        Console.WriteLine(u);
+    }
+}
