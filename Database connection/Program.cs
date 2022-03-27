954178566f9597770d8b86bf81e8ea8228940824
GreedNeSS <ruslan199126@gmail.com>
2022-03-27 14:09:53 +0300

    Connecting to an existing database

---
+++ Database connection/Program.cs
@@ -0,0 +1,10 @@
+﻿using Database_connection;
+
+Console.WriteLine("***** Connecting to an existing database *****");
+using (usersContext db = new usersContext())
+{
+    Console.WriteLine("Список обьектов:");
+
+    List<User> users = db.Users.ToList();
+    users.ForEach(u => Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}"));
+}
---
+++ Database connection/usersContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Database_connection
+{
+    public partial class usersContext : DbContext
+    {
+        public usersContext()
+        {
+        }
+
+        public usersContext(DbContextOptions<usersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source=F:\\\\\\\\users.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
